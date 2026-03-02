0a007a36474af0f82b4ab475068b4d1db14e5207
rathoreshivamsingh11-tech <rathoreshivamsingh11@gmail.com>
2026-03-02 23:48:29 +0530

    Initial commit: add OOPSBannerApp2

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,40 @@
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+       
+        String o1 = "  *****  ";
+        String o2 = " *     * ";
+        String o3 = " *     * ";
+        String o4 = " *     * ";
+        String o5 = " *     * ";
+        String o6 = " *     * ";
+        String o7 = "  *****  ";
+
+        
+        String p1 = " ******* ";
+        String p2 = " *     * ";
+        String p3 = " *     * ";
+        String p4 = " ******* ";
+        String p5 = " *       ";
+        String p6 = " *       ";
+        String p7 = " *       ";
+
+        String s1 = "  *****  ";
+        String s2 = " *     * ";
+        String s3 = " *       ";
+        String s4 = "  *****  ";
+        String s5 = "       * ";
+        String s6 = " *     * ";
+        String s7 = "  *****  ";
+
+        System.out.println(o1 + "  " + o1 + "  " + p1 + "  " + s1);
+        System.out.println(o2 + "  " + o2 + "  " + p2 + "  " + s2);
+        System.out.println(o3 + "  " + o3 + "  " + p3 + "  " + s3);
+        System.out.println(o4 + "  " + o4 + "  " + p4 + "  " + s4);
+        System.out.println(o5 + "  " + o5 + "  " + p5 + "  " + s5);
+        System.out.println(o6 + "  " + o6 + "  " + p6 + "  " + s6);
+        System.out.println(o7 + "  " + o7 + "  " + p7 + "  " + s7);
+    }
+}
