66d7a2e5df9b090aa453bab72043d48a1a46e4e9
rathoreshivamsingh11-tech <rathoreshivamsingh11@gmail.com>
2026-03-02 23:58:22 +0530

    Initial commit: add OOPSBannerApp3

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,54 @@
+.0
+ 
+public class OOPSBannerApp {
+
+   
+    public static void main(String[] args) {
+
+       
+        String[] O = {
+            "  *****  ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            "  *****  "
+        };
+
+        
+        String[] P = {
+            " ******* ",
+            " *     * ",
+            " *     * ",
+            " ******* ",
+            " *       ",
+            " *       ",
+            " *       "
+        };
+
+      
+        String[] S = {
+            "  *****  ",
+            " *     * ",
+            " *       ",
+            "  *****  ",
+            "       * ",
+            " *     * ",
+            "  *****  "
+        };
+
+       
+        for (int i = 0; i < 7; i++) {
+
+            String line = String.join(" ",
+                    O[i],
+                    O[i],
+                    P[i],
+                    S[i]
+            );
+
+            System.out.println(line);
+        }
+    }
+}
